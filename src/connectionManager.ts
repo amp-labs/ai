c040a40ecff825b616f609bf5eedca622524d958
Jatin Sandilya <sandilya.jatin@gmail.com>
2025-03-28 17:45:39 +0530

    feat: `check_connection` tool

--- src/connectionManager.ts
+++ src/connectionManager.ts
@@ -1,15 +1,53 @@
 import { z } from "zod";
 import { Server } from "@modelcontextprotocol/sdk/server/index.js";
 
-export async function createConnectionManagerTools(server: Server, provider: string): Promise<void> {
-    // @ts-ignore
-    server.tool('check-connection', `Check if the connection for ${provider} exists`, {
-        query: z.string(),
-    }, async ({ query }: { query: string }) => {
-        const content = 'connection manager tool'
+export async function createConnectionManagerTools(
+  server: Server,
+  provider: string
+): Promise<void> {
+  // @ts-ignore
+  server.tool(
+    "check-connection",
+    `Check if there is an active connection for ${provider}`,
+    {
+      query: z.string(),
+    },
+    async ({ query }: { query: string }) => {
+      const options = {
+        method: "GET",
+        headers: { "X-Api-Key": process.env.AMPERSAND_API_KEY || "" },
+      };
 
-        return {
-            content,
-        };
-    });
-} 
+      try {
+        const response = await fetch(
+          `https://api.withampersand.com/v1/projects/${process.env.AMPERSAND_PROJECT_ID}/connections?provider=${provider}`,
+          options
+        );
+        const data = await response.json();
+        console.log("[DEBUG] connection response", data);
+
+        if (data.length > 0) {
+          return {
+            content: [
+              {
+                type: "text",
+                text: `Connection found for ${provider}`,
+              },
+            ],
+          };
+        } else {
+          return {
+            content: [
+              {
+                type: "text",
+                text: `No existing connections found for ${provider}`,
+              },
+            ],
+          };
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  );
+}
