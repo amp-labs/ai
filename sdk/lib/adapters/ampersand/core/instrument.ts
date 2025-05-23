4d71fce424b9787aa44ea56e5c4e7f4a061e9336
Jatin Sandilya <sandilya.jatin@gmail.com>
2025-05-23 15:29:01 -0600

    enable instrumentation

--- sdk/lib/adapters/ampersand/core/instrument.ts
+++ sdk/lib/adapters/ampersand/core/instrument.ts
@@ -1,4 +1,4 @@
 import * as Sentry from "@sentry/node";
 
 // NOTE:REPLACE WITH AMPERSAND SENTRY DSN before publishing
-Sentry.init({dsn: undefined});
+Sentry.init({dsn: "https://c64482dcf88414a2e587b62e8ffe9b17@o4508846225162240.ingest.us.sentry.io/4509369063047168"});
